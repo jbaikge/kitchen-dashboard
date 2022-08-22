c7f1a61eeb25770809e6658091c31567be442925
jake <bigjake2s@gmail.com>
2022-08-22 15:12:38 -0400

    Adding calendar fetching

---
+++ dashboard/calendar.go
@@ -0,0 +1,90 @@
+package dashboard
+
+import (
+	"fmt"
+	"time"
+)
+
+type CalendarEvent struct {
+	Summary     string
+	Description string
+	Location    string
+	Start       time.Time
+	End         time.Time
+}
+
+type Calendar struct {
+	Title  string
+	Key    string
+	Events []CalendarEvent
+}
+
+type CalendarEntry struct {
+	Summary     string
+	Description string
+	Location    string
+	Start       struct {
+		Date     string
+		DateTime string
+	}
+	End struct {
+		Date     string
+		DateTime string
+	}
+}
+
+func (entry CalendarEntry) GetStart(loc *time.Location) (t time.Time) {
+	if value := entry.Start.Date; value != "" {
+		t, _ = time.ParseInLocation("2006-01-02", value, loc)
+		return
+	}
+	if value := entry.Start.DateTime; value != "" {
+		t, _ = time.ParseInLocation(time.RFC3339, value, loc)
+	}
+	return
+}
+
+func (entry CalendarEntry) GetEnd(loc *time.Location) (t time.Time) {
+	if value := entry.End.Date; value != "" {
+		t, _ = time.ParseInLocation("2006-01-02", value, loc)
+		return
+	}
+	if value := entry.End.DateTime; value != "" {
+		t, _ = time.ParseInLocation(time.RFC3339, value, loc)
+	}
+	return
+}
+
+func GetCalendars(config Config) (calendars []Calendar, err error) {
+	calendars = make([]Calendar, len(config.Calendars))
+
+	ha := NewHomeAssistant(config)
+	tz := config.TimeZone()
+	for i, calConfig := range config.Calendars {
+		key := fmt.Sprintf("calendar.%s", calConfig.Key)
+		start := time.Now()
+		end := start.Add(calConfig.Outlook)
+		entries := make([]CalendarEntry, 0, 30)
+		if err = ha.GetCalendar(key, start, end, &entries); err != nil {
+			err = fmt.Errorf("fetching calendar [%s]: %w", calConfig.Key, err)
+			return
+		}
+
+		events := make([]CalendarEvent, 0, len(entries))
+		for _, entry := range entries {
+			events = append(events, CalendarEvent{
+				Summary:     entry.Summary,
+				Description: entry.Description,
+				Location:    entry.Location,
+				Start:       entry.GetStart(tz),
+				End:         entry.GetEnd(tz),
+			})
+		}
+
+		calendars[i].Title = calConfig.Title
+		calendars[i].Key = calConfig.Key
+		calendars[i].Events = events
+	}
+
+	return
+}
--- dashboard/home-assistant.go
+++ dashboard/home-assistant.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"net/http"
 	"net/url"
+	"time"
 )
 
 type HomeAssistant struct {
@@ -15,6 +16,42 @@ func NewHomeAssistant(config Config) HomeAssistant {
 	return HomeAssistant{
 		config: config,
 	}
+}
+
+// Fetches a calendar by the provided key and decodes the array into the entries argument
+func (ha HomeAssistant) GetCalendar(key string, start time.Time, end time.Time, entries interface{}) (err error) {
+	base, err := url.Parse(ha.config.HomeAssistant.Endpoint)
+	if err != nil {
+		return fmt.Errorf("parsing endpoint: %w", err)
+	}
+
+	target := base.JoinPath("calendars", key)
+
+	query := url.Values{}
+	query.Add("start", start.Format(time.RFC3339))
+	query.Add("end", end.Format(time.RFC3339))
+	target.RawQuery = query.Encode()
+
+	fmt.Println(target.String())
+
+	request, err := http.NewRequest(http.MethodGet, target.String(), nil)
+	if err != nil {
+		return fmt.Errorf("creating request: %w", err)
+	}
+
+	request.Header.Add("Authorization", "Bearer "+ha.config.HomeAssistant.Token)
+
+	response, err := http.DefaultClient.Do(request)
+	if err != nil {
+		return fmt.Errorf("executing request: %w", err)
+	}
+	defer response.Body.Close()
+
+	if response.StatusCode != http.StatusOK {
+		return fmt.Errorf("bad return: [%d] %s", response.StatusCode, response.Status)
+	}
+
+	return json.NewDecoder(response.Body).Decode(entries)
 }
 
 // Fetches a state by the provided key and decodes it into the state argument
