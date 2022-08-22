8e9ab2c64e1de28d551b8e24f839178a2e46fdfe
jake <bigjake2s@gmail.com>
2022-08-22 10:51:48 -0400

    Adding check for the returned status code

--- dashboard/home-assistant.go
+++ dashboard/home-assistant.go
@@ -22,20 +22,24 @@ func (ha HomeAssistant) GetState(key string, state interface{}) (err error) {
 	target, err := url.JoinPath(ha.config.HomeAssistant.Endpoint, "states", key)
 	fmt.Println(target)
 	if err != nil {
-		return fmt.Errorf("problem creating URL: %w", err)
+		return fmt.Errorf("creating URL: %w", err)
 	}
 	request, err := http.NewRequest(http.MethodGet, target, nil)
 	if err != nil {
-		return fmt.Errorf("problem creating request: %w", err)
+		return fmt.Errorf("creating request: %w", err)
 	}
 
 	request.Header.Add("Authorization", "Bearer "+ha.config.HomeAssistant.Token)
 
 	response, err := http.DefaultClient.Do(request)
 	if err != nil {
-		return fmt.Errorf("problem executing request: %w", err)
+		return fmt.Errorf("executing request: %w", err)
+	}
+	defer response.Body.Close()
+
+	if response.StatusCode != http.StatusOK {
+		return fmt.Errorf("bad return: [%d] %s", response.StatusCode, response.Status)
 	}
 
-	defer response.Body.Close()
 	return json.NewDecoder(response.Body).Decode(state)
 }
