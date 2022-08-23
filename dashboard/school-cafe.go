65f85719d0bc8eb444e9bf5bd3a93ab51d231a72
jake <bigjake2s@gmail.com>
2022-08-23 09:05:34 -0400

    Adding preliminary SchoolCafe integration

---
+++ dashboard/school-cafe.go
@@ -0,0 +1,79 @@
+package dashboard
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"net/url"
+	"time"
+)
+
+const (
+	SchoolCafeEntree    = "ENTREE"
+	SchoolCafeDaily     = "DAILY OFFERINGS"
+	SchoolCafeGrain     = "GRAIN"
+	SchoolCafeVegetable = "VEGETABLE"
+	SchoolCafeFruit     = "FRUIT"
+	SchoolCafeMilk      = "MILK"
+)
+
+const (
+	SchoolCafeMealTypeLunch = "Lunch"
+)
+
+type SchoolCafeItem struct {
+	Description string `json:"MenuItemDescription"`
+}
+
+type SchoolCafe struct {
+	config Config
+}
+
+func NewSchoolCafe(config Config) SchoolCafe {
+	return SchoolCafe{
+		config: config,
+	}
+}
+
+func (sc SchoolCafe) GetMenuItems(date time.Time, mealType string) (items map[string][]SchoolCafeItem, err error) {
+
+	base, err := url.Parse(sc.config.SchoolLunch.Endpoint)
+	if err != nil {
+		err = fmt.Errorf("parsing base URL: %w", err)
+		return
+	}
+
+	target := base.JoinPath("CalendarView", "GetDailyMenuitemsByGrade")
+
+	query := url.Values{}
+	query.Set("SchoolId", sc.config.SchoolLunch.SchoolId)
+	query.Set("ServingDate", date.Format("01/02/2006"))
+	query.Set("ServingLine", "Main Line")
+	query.Set("MealType", mealType)
+	query.Set("Grade", fmt.Sprintf("%02d", sc.config.SchoolLunch.Grade))
+	query.Set("PersonId", "null")
+	target.RawQuery = query.Encode()
+
+	log.Printf("%s", target.String())
+	request, err := http.NewRequest(http.MethodGet, target.String(), nil)
+	if err != nil {
+		err = fmt.Errorf("creating request: %w", err)
+		return
+	}
+
+	http.DefaultClient.Timeout = 10 * time.Second
+	response, err := http.DefaultClient.Do(request)
+	if err != nil {
+		err = fmt.Errorf("performing request: %w", err)
+		return
+	}
+	defer response.Body.Close()
+
+	items = make(map[string][]SchoolCafeItem)
+	if err = json.NewDecoder(response.Body).Decode(&items); err != nil {
+		return
+	}
+
+	return
+}
---
+++ dashboard/school-lunch.go
@@ -0,0 +1,34 @@
+package dashboard
+
+import (
+	"fmt"
+	"time"
+)
+
+type SchoolMenuItem struct {
+	Description string
+}
+
+type SchoolMenu struct {
+	Entrees []SchoolMenuItem
+}
+
+func GetSchoolLunch(config Config) (menu SchoolMenu, err error) {
+	cafe := NewSchoolCafe(config)
+	entries, err := cafe.GetMenuItems(time.Now(), SchoolCafeMealTypeLunch)
+	if err != nil {
+		return
+	}
+
+	entrees, ok := entries[SchoolCafeEntree]
+	if !ok {
+		err = fmt.Errorf("menu items of type %s not found", SchoolCafeEntree)
+	}
+
+	menu.Entrees = make([]SchoolMenuItem, len(entrees))
+	for i, entree := range entrees {
+		menu.Entrees[i].Description = entree.Description
+	}
+
+	return
+}
