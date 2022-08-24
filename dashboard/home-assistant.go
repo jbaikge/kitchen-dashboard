932edde893170fbd0adf007275d3306b9c94a670
jake <bigjake2s@gmail.com>
2022-08-24 10:47:40 -0400

    Adding support for calling services

--- dashboard/home-assistant.go
+++ dashboard/home-assistant.go
@@ -1,6 +1,7 @@
 package dashboard
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
 	"net/http"
@@ -76,4 +77,37 @@ func (ha HomeAssistant) GetState(key string, state interface{}) (err error) {
 	}
 
 	return json.NewDecoder(response.Body).Decode(state)
+}
+
+func (ha HomeAssistant) CallService(domain string, service string, payload interface{}) (err error) {
+	base, err := url.Parse(ha.config.HomeAssistant.Endpoint)
+	if err != nil {
+		return fmt.Errorf("parsing endpoint: %w", err)
+	}
+
+	target := base.JoinPath("services", domain, service)
+
+	buffer := new(bytes.Buffer)
+	if err = json.NewEncoder(buffer).Encode(payload); err != nil {
+		return fmt.Errorf("encoding payload: %w", err)
+	}
+
+	request, err := http.NewRequest(http.MethodPost, target.String(), buffer)
+	if err != nil {
+		return fmt.Errorf("creating request: %w", err)
+	}
+	request.Header.Set("Authorization", "Bearer "+ha.config.HomeAssistant.Token)
+	request.Header.Set("Content-Type", "application/json")
+
+	response, err := http.DefaultClient.Do(request)
+	if err != nil {
+		return fmt.Errorf("executing request: %w", err)
+	}
+	defer response.Body.Close()
+
+	if response.StatusCode != http.StatusOK {
+		return fmt.Errorf("bad return: [%d] %s", response.StatusCode, response.Status)
+	}
+
+	return
 }
